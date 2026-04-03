1977a80c4114a05d022f7718dccebe2a5a1beb9a
Diego Vargas <dvargasv8@correo.uss.cl>
2026-04-03 18:36:44 -0300

    solemne 1

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ app = FastAPI()
 
 @app.get("/time")
 def get_current_time():
-    # Obtenemos la hora del sistema y le damos el formato pedido
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # Devolvemos el diccionario con la llave "datetime" para cumplir tu esquema
+   
     return {"datetime": now}
